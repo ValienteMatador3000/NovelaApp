b7bcb25112bb997a3ada92bdf7b1ccc5c0da1c34
ValienteMatador3000 <jozuemar1234@gmail.com>
2025-12-28 22:59:10 -0600

    Update jobs.py

--- backend/jobs.py
+++ backend/jobs.py
@@ -23,3 +23,25 @@ def crear_job(func, *args, **kwargs):
 
     threading.Thread(target=wrapper, daemon=True).start()
     return job_id
+
+
+def job_audio(func, *args):
+    job_id = crear_id()
+
+    jobs[job_id] = {
+        "estado": "procesando",
+        "resultado": None,
+        "error": None
+    }
+
+    def wrapper():
+        try:
+            resultado = func(*args)
+            jobs[job_id]["estado"] = "completado"
+            jobs[job_id]["resultado"] = resultado
+        except Exception as e:
+            jobs[job_id]["estado"] = "error"
+            jobs[job_id]["error"] = str(e)
+
+    Thread(target=wrapper, daemon=True).start()
+    return job_id
